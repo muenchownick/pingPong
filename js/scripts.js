fdfe169659b6939e6cc225dd4d90605aa24049b7
Nickolas Muenchow <muenchownick@gmail.com>
2016-04-01 12:37:10 -0700

    formatting the javascript, when you enter text, the input resets

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,34 @@
+// business logic
 var ping = function(number) {
-if (number % 3 === 0 && number % 15 != 0) {
+  // this spits ping \/
+if ((0 < number && number < 100) && number % 3 === 0 && number % 15 != 0) {
   $("#results").append("<li>ping</li>");
-} else if (number % 5 === 0 && number % 15 != 0) {
+  // this spits pong \/
+} else if ((0 < number && number < 100) && number % 5 === 0 && number % 15 != 0) {
   $("#results").append("<li>pong</li>");
-} else if (number % 15 === 0) {
+  // this spits pingpong \/
+} else if ((0 < number && number < 100) && number % 15 === 0) {
   $("#results").append("<li>pingpong</li>");
-} else if (number % 3 != 0 && number % 5 != 0 && number % 15 != 0 ){
-  $("#results").append(number);
-  $("#results").append("<br>");
-
+  // this is just spitting regular numbers \/
+} else if ((0 < number && number < 100) && number % 3 != 0 && number % 5 != 0 && number % 15 != 0 && number != 0 ){
+  $("#results").append(
+    $("<li>").append(number)
+  );
 }
+// this is spitting bad input message \/
   else {
+    $("#results").append("<li>bad input</li>");
     return false;
   }
 };
+
+// to clear the pages text function *not working
+function clearText() {
+    document.getElementById('results').value="";
+};
+// breaker
+// user interface logic
+// breaker
 
 $(document).ready(function(){
   $("form#pingPong").submit(function(event) {
@@ -21,6 +36,13 @@ $(document).ready(function(){
 
     var number = $("#pingPongs").val();
     var result = ping(number);
-    $("#result").text(result);
+
+    $("form#pingPong")[0].reset();
   });
 });
+
+// not working at the moment?
+    // $("#myform")[0].reset();
+// two ways to reset inputform
+    // $("form#pingPong").each(function(){
+    //     this.reset();
