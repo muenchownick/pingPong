83ba0e9d27307dd82dd79ac36a70bfb30936ffda
Nickolas Muenchow <muenchownick@gmail.com>
2016-04-01 09:33:26 -0700

    ping pong function working, no errors

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,36 @@
-$document.ready(function(){
-  
+var ping = function(number) {
+if (number % 3 === 0 && number % 15 != 0) {
+  $("#results").append("<li>ping</li>");
+} else if (number % 5 === 0 && number % 15 != 0) {
+  $("#results").append("<li>pong</li>");
+} else if (number % 15 === 0) {
+  $("#results").append("<li>pingpong</li>");
+} else if (number % 3 != 0 && number % 5 != 0 && number % 15 != 0 ){
+  $("#results").append(number);
+}
+  else {
+    return false;
+  }
+};
+
+$(document).ready(function(){
+  $("form#pingPong").submit(function(event) {
+    event.preventDefault();
+
+    var number = $("#pingPongs").val();
+    var result = ping(number);
+    $("#result").text(result);
+    alert(number);
+
+
+
+
+  });
+
+
+
+
+
+
+
 });
