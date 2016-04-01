e9d62e24c5b1ac30396b73bdab8d129d08426b2b
Nickolas Muenchow <muenchownick@gmail.com>
2016-04-01 14:26:40 -0700

    code all cleaned up, fully working pingpong application

--- js/scripts.js
+++ js/scripts.js
@@ -13,13 +13,11 @@ var ping = function (number) {
     }
   };
 };
-// to clear the pages text function
+// to clear the #result div text //
 function clearText() {
     document.getElementById("results").innerHTML="";
 };
-
 // ****** user interface logic ****** //
-
 $(document).ready(function(){
   $("form#pingPong").submit(function(event) {
     event.preventDefault();
@@ -29,27 +27,3 @@ $(document).ready(function(){
     $("form#pingPong")[0].reset();
   });
 });
- // ************************* OLD CODE *************************
- // ************************* NO LOOP  *************************
-// var ping = function(number) {
-//   // this spits ping \/
-// if ((0 < number && number < 100) && number % 3 === 0 && number % 15 != 0) {
-//   $("#results").append("<li>ping</li>");
-//   // this spits pong \/
-// } else if ((0 < number && number < 100) && number % 5 === 0 && number % 15 != 0) {
-//   $("#results").append("<li>pong</li>");
-//   // this spits pingpong \/
-// } else if ((0 < number && number < 100) && number % 15 === 0) {
-//   $("#results").append("<li>pingpong</li>");
-//   // this is just spitting regular numbers \/
-// } else if ((0 < number && number < 100) && number % 3 != 0 && number % 5 != 0 && number % 15 != 0 && number != 0 ){
-//   $("#results").append(
-//     $("<li>").append(number)
-//   );
-// }
-// this is spitting bad input message \/
-//   else {
-//     $("#results").append("<li>bad input</li>");
-//     return false;
-//   }
-// };
